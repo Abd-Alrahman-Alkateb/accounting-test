269466aa3806da1a9ceb0ec8687dc30466a40de1
abd_alrahman <76664726+Abd-Alrahman-Alkateb@users.noreply.github.com>
2023-09-24 15:57:52 +0300

    init commit

---
+++ app/Http/Controllers/CostController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cost;
+use Illuminate\Http\Request;
+
+class CostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $costs = Cost::all();
+        return response()->json([
+            'costs' => $costs
+        ], 200);
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validation = $request ->validate([
+            'title'   => 'required|min:2|max:20',
+            'post_id' => 'required|numeric|exists:posts,id'
+        ]);
+
+        $cost= Cost::create($validation);
+        if ($cost){
+            return response()->json([
+                'message' => "Store created successfully",
+            ], 201);
+        }
+        return response()->json([
+            'message' => 'Error',
+        ], 400);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cost  $cost
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cost $cost)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cost  $cost
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cost $cost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cost  $cost
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cost $cost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cost  $cost
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cost $cost)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return response()->json([
+            'posts' => $posts
+        ], 200);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validation = $request ->validate([
+            'title' => 'required|min:2|max:20',
+        ]);
+
+        $post= Post::create($validation);
+        if ($post){
+            return response()->json([
+                'message' => "Store created successfully",
+            ], 201);
+        }
+        return response()->json([
+            'message' => 'Error',
+        ], 400);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
